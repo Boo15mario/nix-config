6ef00419525efc7d71dbfcf4f4176809b58ddf4b
boo15mario <bala.alek@gmail.com>
2026-01-18 14:46:28 -0600

    added the hashese

--- access.nix
+++ access.nix
@@ -10,11 +10,11 @@ let
       repo = "access-launcher";
       rev = "b8e047528c2d91ae6bbc6f31031d1e67908d8f6c";
       # TODO: Replace with correct hash after first build failure
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      hash = "sha256-ScpC6uOZ06UTzyI8Zkp1qsPnG2zllklM5KefkbJdwiA=";
     };
 
     # TODO: Replace with correct hash after first build failure
-    cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    cargoHash = "sha256-Yri+MWl28/N36MPweGQBOBZSmyC3L89anXe5kwITIxY=";
 
     nativeBuildInputs = [ pkgs.pkg-config ];
     buildInputs = [ pkgs.gtk4 ];
@@ -29,11 +29,11 @@ let
       repo = "universal-startup-manager";
       rev = "a1f030e321b4dff38d8e14e3e200d81a223a9315";
       # TODO: Replace with correct hash after first build failure
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      hash = "sha256-uQ0VorRU02af5cxMODRWusokqWG9JbwTNstlFv3RIJ4=";
     };
 
     # TODO: Replace with correct hash after first build failure
-    cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    cargoHash = "sha256-Dr27mzPiSmkeTnmHTDgDnkmThq+AkZ6KFHoFf2645uk=";
 
     nativeBuildInputs = [ pkgs.pkg-config ];
     buildInputs = [ pkgs.gtk4 ];
