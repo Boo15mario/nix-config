ea0869f9a664901c01923c90db275ce471e49a55
boo15mario <bala.alek@gmail.com>
2026-02-26 16:05:01 -0600

    Force-update access-nix dependency to latest fixed commit and pin via SHA256

--- boo76/access.nix
+++ boo76/access.nix
@@ -4,7 +4,10 @@
     nixpkgs.overlays = [
     (final: prev:
       let
-        accessPkgs = import (builtins.fetchTarball "https://github.com/boo15mario/access-nix/archive/main.tar.gz") {
+        accessPkgs = import (builtins.fetchTarball {
+          url = "https://github.com/boo15mario/access-nix/archive/5794f74202dbf277cef4dde3d59f2a57b10e06f9.tar.gz";
+          sha256 = "0nj801maiijxqn35pqmnaffr8iqc39ick6z6440hipdb1bxci7pg";
+        }) {
           pkgs = prev;
         };
       in
